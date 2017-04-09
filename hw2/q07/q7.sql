987eb08617a714b7c08a18f3c990c7daa0575456
Jay Lin <jay101630@gmail.com>
2017-04-09 16:50:25 +0800

    fix q7

--- hw2/q07/q7.sql
+++ hw2/q07/q7.sql
@@ -1,14 +1,7 @@
 USE `lahman2016`;
 
 SELECT
-    S.yearID, AVG(S.salary)
-FROM
-    Salaries S
-GROUP BY
-    S.yearID;
-    
-SELECT
-    S.yearID, MAX(S.salary)
+    S.yearID, AVG(S.salary), MAX(S.salary)
 FROM
     Salaries S
 GROUP BY
